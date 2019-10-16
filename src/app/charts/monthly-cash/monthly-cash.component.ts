34a4aaa270ba5e7ddfbc4a8deb0fd5ff20b0ea31
Michael Hladky <michael@hladky.at>
2019-10-16 21:32:06 +0200

    removed dataService as not used
    I removed the data service and import as it was not used

--- src/app/charts/monthly-cash/monthly-cash.component.ts
+++ src/app/charts/monthly-cash/monthly-cash.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { DataService } from 'src/app/services/data.service';
 import { UIChart } from 'primeng/chart';
 
 @Component({
@@ -56,7 +55,7 @@ export class MonthlyCashComponent implements OnInit {
     labels: string[];
     datasets: { label: string; data: number[]; backgroundColor: string[] }[];
   };
-  constructor(private service: DataService) {}
+  constructor() {}
 
   ngOnInit() {
     // const observableB = this.service.wsSubject().multiplex(
