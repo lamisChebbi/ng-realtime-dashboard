417529e193570f9bfbb0ed43225bb6aabb06e08e
Michael Hladky <michael@hladky.at>
2019-10-16 22:05:41 +0200

    Refactoring related to data.service.ts
    Explanation in the comments in the constructor

--- src/app/live-table/live-table.component.ts
+++ src/app/live-table/live-table.component.ts
@@ -21,13 +21,17 @@ import { tap, map } from 'rxjs/operators';
 export class LiveTableComponent implements OnInit, OnDestroy {
 
 
-   trade$= this.service.dataUpdates$().pipe(
+   trade$ = this.service.socket$.pipe(
     map(rows => rows.data),
     tap({ error: err => console.log(err), 
       complete: () => console.log('Connexion Closed') })
   );  
 
   constructor(private service: DataService) {
+    // Refactoring for the fix in the data.service
+    // ATM the component is responsible for the connection. 
+    // I would suggest moving the init part into app.component and passing data as input binding to here
+    this.service.connect();
   }
 
   updateStatus(trade) {
