0bd49e963d0baa7944f67b1c03e7312e3b432a14
Lamis Chebbi <lchebbi@vermeg.com>
2020-02-16 21:12:02 +0100

    feat(): Add Support to the reconnection mechanism to WebSockets subjects

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,30 +1,79 @@
-import { Injectable, Inject } from '@angular/core';
-import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
-import {environment} from '../../environments/environment';
+import { Injectable } from '@angular/core';
+import { webSocket } from 'rxjs/webSocket';
+import { environment } from '../../environments/environment';
+import { Observable, timer, Subject, EMPTY } from 'rxjs';
+import { retryWhen, tap, delayWhen, switchAll, catchError } from 'rxjs/operators';
 export const WS_ENDPOINT = environment.wsEndpoint;
+export const RECONNECT_INTERVAL = environment.reconnectInterval;
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  private socket$: WebSocketSubject<any>;
-  dataUpdates$ = this.socket$.asObservable();
-  
-  constructor() {}
 
-   public connect(): WebSocketSubject<any> {
-    if (!this.socket$ || this.socket$.closed) {
-      this.socket$ = webSocket(WS_ENDPOINT);
-    }
-    return this.socket$;
+  private socket$;
+  private messagesSubject$ = new Subject();
+  public messages$ = this.messagesSubject$.pipe(switchAll(), catchError(e => { throw e; }));
+
+  constructor() {
   }
 
-  closeConnection() {
+  /**
+   * Creates a new WebSocket subject and send it to the messages subject
+   * @param cfg if true the observable will be retried.
+   */
+  public connect(cfg: { reconnect: boolean } = { reconnect: false }): void {
+
+    if (!this.socket$ || this.socket$.closed) {
+      this.socket$ = this.getNewWebSocket();
+      const messages = this.socket$.pipe(cfg.reconnect ? this.reconnect : o => o,
+        tap({
+          error: error => console.log(error),
+        }), catchError(_ => EMPTY));
+      // toDO only next an observable if a new subscription was made double-check this
+      this.messagesSubject$.next(messages);
+    }
+  }
+
+  /**
+   * Retry a given observable by a time span
+   * @param o the observable to be retried
+   */
+  private reconnect(o: Observable<any>): Observable<any> {
+    return o.pipe(retryWhen(errors => errors.pipe(tap(val => console.log('[Data Service] Try to reconnect', val)),
+      delayWhen(_ => timer(RECONNECT_INTERVAL)))));
+  }
+
+  close() {
     this.socket$.complete();
+    this.socket$ = undefined;
   }
 
   sendMessage(msg: any) {
-     this.socket$.next(msg);
+    this.socket$.next(msg);
+  }
+
+  /**
+   * Return a custom WebSocket subject which reconnects after failure
+   */
+  private getNewWebSocket() {
+    return webSocket({
+      url: WS_ENDPOINT,
+      openObserver: {
+        next: () => {
+          console.log('[DataService]: connection ok');
+        }
+      },
+      closeObserver: {
+        next: () => {
+          console.log('[DataService]: connection closed');
+          this.socket$ = undefined;
+          this.connect({ reconnect: true });
+        }
+      },
+
+    });
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  wsEndpoint: 'ws://localhost:8081'
+  wsEndpoint: 'ws://localhost:8081',
+  reconnectInterval: 2000
 };
 
 /*
